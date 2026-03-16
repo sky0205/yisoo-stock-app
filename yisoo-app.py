87695e325499f81a45405acb1c1c161f83b14faa
sky0205 <kysuh57@gmail.com>
2026-03-16 11:55:02 +0900

    Update yisoo-app.py

--- yisoo-app.py
+++ yisoo-app.py
@@ -141,7 +141,9 @@ if symbol:
                 <div class='trend-item'>● <b>추세 진단:</b> 현재 장부는 <span style='color:#D32F2F;'>{trend_desc}</span> 상태로 판독되구먼요.</div>
                 <div class='trend-item'>● <b>수비 상태:</b> 성벽({format(defense_line, fmt)} {currency}) {'함락! 후퇴하십시오.' if p < defense_line else '사수 중입니다.'}</div>
                <div class='trend-item'>
-    <span style='color:#FF0000; font-weight:bold; font-size:1.2em; background-color:#000000; padding:3px 5px; border-radius:3px;'>{us_advice}</span><br>
+    <div class='trend-item'>
+    ● <b>필살 조언:</b><br>
+    <span style='color:#FF0000; font-weight:bold; font-size:1.2em; background-color:#000000; padding:3px 5px; border-radius:3px; display:inline-block; margin:5px 0;'>{us_advice}</span><br>
     <span class='advice-highlight'>{'⚠️ 신고가 추격 시: ' + format(p*0.95, fmt) + ' ' + currency + ' 이탈 시 손절!'}</span>
 </div>
                 </div>""", unsafe_allow_html=True)
