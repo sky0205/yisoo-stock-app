f004ff076d76e8319de8d2dcbfde9351c3974190
sky0205 <kysuh57@gmail.com>
2026-03-18 14:10:59 +0900

    Update yisoo-app.py

--- yisoo-app.py
+++ yisoo-app.py
@@ -126,14 +126,15 @@ if symbol:
             i1, i2, i3, i4 = st.columns(4)
             with i1: # 127번 줄
         # [v36056] upper와 lower가 누군지 성벽 위에 이름표를 붙여주시게!
-                upper, lower = bband['upperband'].iloc[-1], bband['lowerband'].iloc[-1]
-                mid = (upper + lower) / 2  # 이제야 mid가 계산될 게야.
+                upper = bb['upperband'].iloc[-1]
+                lower = bb['lowerband'].iloc[-1]
+                mid = (upper + lower) / 2
         
                 if p > mid:
                     bb_diag = f"• **[중앙선 수복!]** 빳빳하게 성문 부쉈으니 이제 천정({upper:,.0f}원) 향해 진격하시게."
                 else:
                     bb_diag = f"• **[중앙선 하단]** 아직 성문 밖일세. {mid:,.0f}원 수복 전까지는 보따리 사수하시게."
-        
+            
                 st.markdown(f"<div class='ind-box'><p class='ind-title'>Bollinger (기세)</p><p class='ind-diag'>{bb_diag}</p></div>", unsafe_allow_html=True)
             with i2: # RSI (온도) 기둥일세
                 if rsi_val >= 60:
