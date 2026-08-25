c79e321c8a2acc7658a48044dc6aa7e98ad977fa
sky0205 <kysuh57@gmail.com>
2026-08-25 18:42:02 +0900

    Update yisoo-app.py

--- yisoo-app.py
+++ yisoo-app.py
@@ -495,12 +495,14 @@ if symbol:
                 f"<span style='color:#7B1FA2; font-weight:bold;'>🟣 120일선: {ma120_str}</span><br>"
             )
 
-            if is_squeeze:
-                squeeze_info_str = f"<br>• ⚡ <b>[밴드폭 극초축소({bandwidth:.1f}%)]</b> 에너지가 바짝 응축되었구먼! 얕은 조정 후 폭발할 수 있으니 돌파 시 정면 대응하시게."
+            if is_escape_buy_signal:
+                squeeze_info_str = ""  # 2단계 진바닥 탈출 시에는 밴드폭 경고 문구를 아예 숨김
+            elif is_squeeze:
+                squeeze_info_str = f"<br>• ⚡ <b>[밴드폭 극초축소({bandwidth:.1f}%)]</b> 에너지가 바짝 응축..."
             elif bandwidth < 20.0:
-                squeeze_info_str = f"<br>• 🟡 <b>[밴드폭 협소({bandwidth:.1f}%)]</b> 밴드폭이 20% 미만이오! 눌림목 공략 시 먹을 자리가 부족하니 무리한 진입을 자제하시게."
+                squeeze_info_str = f"<br>• 🟡 <b>[밴드폭 협소({bandwidth:.1f}%)]</b> 밴드폭이 20% 미만이오!..."
             else:
-                squeeze_info_str = f"<br>• 🌊 <b>[밴드폭 넉넉함({bandwidth:.1f}%)]</b> 활주로가 넉넉히 트였으니 정석 눌림목 타점을 공략하시게."
+                squeeze_info_str = f"<br>• 🌊 <b>[밴드폭 넉넉함({bandwidth:.1f}%)]</b> 활주로가 넉넉히 트였..."
 
             is_down_trend_v = (p < prev_p) and (p_chg < 0)
 
